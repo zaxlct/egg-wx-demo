56c8a33ea68154224630669b9d16ca6cbd963e7e
zaxlct <zaxlct@foxmail.com>
2020-03-15 13:01:39 +0800

    swaggerdoc 生效

---
+++ app/contract/reponse/base.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  baseResponse: {
+    result: {
+      type: 'boolean',
+      required: true
+    },
+    errorMessage: {
+      type: 'string'
+    },
+  },
+}
--- app/controller/user.js
+++ app/controller/user.js
@@ -2,11 +2,21 @@
 
 const Controller = require('egg').Controller
 
+/**
+ * @controller user 用户接口
+ */
 class UserController extends Controller {
   // async index() {
 
   // }
 
+  /**
+   * @summary 创建用户
+   * @description 创建用户，记录用户账户/密码/类型
+   * @router post /api/v1/user
+   * @request body createUserRequest *body
+   * @response 200 baseResponse 创建成功
+   */
   async create() {
     const ctx = this.ctx
     const user = await ctx.service.user.create(ctx.request.body)
