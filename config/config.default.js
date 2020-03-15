3b9e2f5604ae8506fa1bc9c109cf518e983d87b4
zaxlct <zaxlct@foxmail.com>
2020-03-15 10:42:51 +0800

    enable csrf

--- config/config.default.js
+++ config/config.default.js
@@ -24,7 +24,11 @@ module.exports = appInfo => {
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1584066474406_4742'
-
+  config.security = {
+    csrf: {
+      enable: false
+    }
+  }
   // add your middleware config here
   config.middleware = []
 
