a08561fb2e8e081537dda0cd83ac828158338c3f
zaxlct <zaxlct@foxmail.com>
2020-03-19 19:09:42 +0800

    取消、点赞时修改点赞数

--- app/service/favor.js
+++ app/service/favor.js
@@ -9,8 +9,9 @@ class FavorService extends Service {
     if (favor) {
       throw new ctx.app.errs.LikeError()
     }
+    let art = null
     try {
-      await ctx.service.classic.getArtById(art_id, type)
+      art = await ctx.service.classic.getArtById(art_id, type)
     } catch (error) {
       throw new ctx.app.errs.LikeError('你点赞的资源不存在')
     }
@@ -19,6 +20,7 @@ class FavorService extends Service {
       type,
       uid,
     })
+    await art.increment('fav_nums')
   }
 
   async deleteFavor(art_id, type, uid) {
@@ -27,9 +29,16 @@ class FavorService extends Service {
     if (!favor) {
       throw new ctx.app.errs.DislikeError()
     }
+    let art = null
+    try {
+      art = await ctx.service.classic.getArtById(art_id, type)
+    } catch (error) {
+      throw new ctx.app.errs.LikeError('你取消点赞资源不存在')
+    }
     await favor.destroy({
       force: true, // 物理删除 or 软删除
     })
+    await art.decrement('fav_nums')
   }
 
   async getFavorByArtId(art_id, type, uid) {
