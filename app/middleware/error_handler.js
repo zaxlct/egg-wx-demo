ab6ebfd45fa5e6d900c8d0b7b3dcd64db2161d52
zaxlct <zaxlct@foxmail.com>
2022-06-27 09:36:42 +0800

    Revert "Revert "方便其它人调用""
    This reverts commit 234fcdb60fcc9e0d2699246f4c9cf5d10e7bd3cd.

--- app/middleware/error_handler.js
+++ app/middleware/error_handler.js
@@ -11,24 +11,26 @@ module.exports = () => {
       const isHttpException = err instanceof HttpException
       const isDev = ctx.app.config.env !== 'prod'
       const status = err.code || 500
-      if (!isHttpException && isDev) {
-        throw err
-      } else if (!isHttpException && !isDev) {
-        // 所有的异常都在 app 上触发一个 error 事件，框架会记录一条错误日志
-        ctx.app.emit('error', err, ctx)
-      }
+      // if (!isHttpException && isDev) {
+      //   throw err
+      // } else if (!isHttpException && !isDev) {
+      //   // 所有的异常都在 app 上触发一个 error 事件，框架会记录一条错误日志
+      //   ctx.app.emit('error', err, ctx)
+      // }
 
       if (isHttpException) {
         ctx.body = {
           msg: err.msg,
           erorr_code: err.errorCode,
-          request: `${ctx.method} ${ctx.path}`
+          request: `${ctx.method} ${ctx.path}`,
+          ...err,
         }
       } else {
         ctx.body = {
           msg: 'we made a mistake O(∩_∩)O~~',
           error_code: 999,
-          request: `${ctx.method} ${ctx.path}`
+          request: `${ctx.method} ${ctx.path}`,
+          ...err,
         }
       }
       ctx.status = status
